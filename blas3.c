e64d3aab38ddd3f7ece6f3c9ab0cea69c985ff39
Võ Văn Nghĩa <vanvnghia@gmail.com>
2021-12-21 21:14:15 +0100

    initial version of blocking cache

--- blas3.c
+++ blas3.c
@@ -123,14 +123,23 @@ void blocking_dot(double *A, int lda, double *B, int ldb, double *C, int ldc) {
 #pragma omp parallel for schedule(runtime) default(shared) private(i, j, k, \
                                                                    ii, jj, kk)
 #endif
-  for (k = 0; k < K; k += BLOCK)
-    for (j = 0; j < N; j += BLOCK)
-      for (i = 0; i < M; i += BLOCK)
-        for (kk = 0; kk < fmin(K - k, BLOCK); kk++)
-          for (jj = 0; jj < fmin(N - j, BLOCK); jj++)
-            for (ii = 0; ii < fmin(M - i, BLOCK); ii++)
+  for (k = 0; k < K; k += BLOCK) {
+    for (j = 0; j < N; j += BLOCK) {
+      for (i = 0; i < M; i += BLOCK) {
+        int kmin = fmin(K - k, BLOCK);
+        for (kk = 0; kk < kmin; kk++) {
+          int jmin = fmin(N - j, BLOCK);
+          for (jj = 0; jj < jmin; jj++) {
+            int imin = fmin(M - i, BLOCK);
+            for (ii = 0; ii < imin; ii++) {
               C[(ii + i) + ldc * (jj + j)] +=
                   A[(ii + i) + lda * (kk + k)] * B[(kk + k) + ldb * (jj + j)];
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 int main() {
